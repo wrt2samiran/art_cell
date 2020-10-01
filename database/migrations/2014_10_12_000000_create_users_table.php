ab644d61c1fd94ee613e258e1c06db42d28d265f
Samiran <git@pixelsolutionz.in>
2020-10-01 15:33:41 +0530

    migration for roles and users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,27 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string(('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('phone')->nullable();
+            $table->foreign('role_id')->references('id')->on('roles');
+            $table->string('userkey')->nullable();
+            
+            $table->enum('usertype', ['S', 'SA', 'FU'])->nullable()->comment = 'S=Superadmin SA=SubAdmin FU=Frontend User';
+            $table->string(('profile_pic')->nullable();
+            $table->string('password')->nullable();
+            $table->enum('status', ['A', 'I', 'D'])->comment = 'A-active,I-inactive,D-delete';
+            $table->longText('setting_json')->nullable();
             $table->rememberToken();
+            $table->string('api_token')->nullable();
+            $table->enum('created_from', ['B', 'F'])->nullable()->comment = 'B= Backend , F = Frontednd';
             $table->timestamps();
+            $table->bigInteger('created_by')->length(20);
+            $table->bigInteger('updated_by')->length(20);
+            $table->enum('is_deleted', ['Y', 'N'])->default('N')->comment = 'Y-yes,N-no';
+            $table->bigInteger('deleted_by')->length(20)->nullable();
+            $table->softDeletes('deleted_at', 0)->nullable();
         });
     }
 
