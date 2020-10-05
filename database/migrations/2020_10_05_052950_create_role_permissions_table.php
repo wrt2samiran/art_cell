0b7db2f4ca36e1132755d0c45c82c9a7dca68321
Samiran <git@pixelsolutionz.in>
2020-10-05 19:04:48 +0530

    DB migration done

---
+++ database/migrations/2020_10_05_052950_create_role_permissions_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolePermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role_permissions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->unsignedBigInteger('module_id');
+            $table->foreign('module_id')->references('id')->on('modules')->onDelete('cascade');
+            $table->unsignedBigInteger('module_functionality_id');
+            $table->foreign('module_functionality_id')->references('id')->on('module_functionalities')->onDelete('cascade');
+            $table->enum('status', ['A', 'I', 'D'])->comment = 'A-active,I-inactive,D-delete';
+            $table->enum('is_deleted', ['Y', 'N'])->default('N')->comment = 'Y-yes,N-no';
+            $table->timestamps();
+            $table->bigInteger('created_by');
+            $table->bigInteger('updated_by');
+            $table->bigInteger('deleted_by')->nullable();
+            $table->softDeletes('deleted_at', 0)->nullable();  
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('role_permissions');
+    }
+}
---
+++ database/migrations/2020_10_05_103605_create_tasks_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('contarct_id');
+            $table->foreign('contarct_id')->references('id')->on('contracts')->onDelete('cascade');
+            $table->string('task_name');
+            $table->enum('task_type', ['O', 'M'])->comment = 'O=>One Time, M=>Maintenance';
+            $table->string('pre_reminder_frequency');
+            $table->integer('maintenance_frequency');
+            $table->date('reminder_date');
+            $table->date('task_start_date');
+            $table->integer('task_total_days');
+            $table->date('task_end_date');
+            $table->enum('status', ['A', 'I', 'D'])->comment = 'A-active,I-inactive,D-delete';
+            $table->enum('po_continue_service', ['Y', 'N'])->default('N')->comment = 'Y-yes,N-no';
+            $table->enum('admin_continue_service', ['Y', 'N'])->default('N')->comment = 'Y-yes,N-no';           
+            $table->timestamps();
+            $table->bigInteger('created_by');
+            $table->bigInteger('updated_by');
+            $table->enum('is_deleted', ['Y', 'N'])->default('N')->comment = 'Y-yes,N-no';
+            $table->bigInteger('deleted_by')->nullable();
+            $table->softDeletes('deleted_at', 0)->nullable(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
